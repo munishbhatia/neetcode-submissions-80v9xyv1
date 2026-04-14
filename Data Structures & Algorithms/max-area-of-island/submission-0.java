8930535c947a6875fa3d3566aa3dea98285216c4
Mickey Bhatia <munish.bhatia.cse@gmail.com>
2026-04-14 12:08:36 -0400

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    final int[][] directions = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if(grid == null) return 0;
+
+        final int rows = grid.length;
+        final int cols = grid[0].length;
+
+        if(rows == 0 || cols == 0) return 0;
+
+        int maxSize = 0;
+
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0; j < cols; ++j) {
+                if(grid[i][j] == 1) {
+                    int size = dfs(grid, rows, cols, i, j);
+                    maxSize = Math.max(size, maxSize);
+                }
+            }
+        }
+
+        return maxSize;
+    }
+
+    private int dfs(int[][] grid, int rows, int cols, int row, int col) {
+        grid[row][col] = 0; //marking this cell as visited
+
+        int size = 1;
+
+        for(int[] dir : directions) {
+            int neighbourRow = row + dir[0];
+            int neighbourCol = col + dir[1];
+
+            if(neighbourRow < 0 || neighbourRow >= rows || neighbourCol < 0 || neighbourCol >= cols) continue;
+
+            if(grid[neighbourRow][neighbourCol] == 1) {
+                size += dfs(grid, rows, cols, neighbourRow, neighbourCol);
+            }
+        }
+
+        return size;
+    }
+}

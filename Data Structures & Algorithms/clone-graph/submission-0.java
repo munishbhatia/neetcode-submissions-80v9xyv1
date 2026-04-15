c196ff8ca02c10bd166be6258d86085692e65699
Mickey Bhatia <munish.bhatia.cse@gmail.com>
2026-04-15 10:30:34 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,50 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+
+        final Map<Integer, Node> valCloneNodeMap = new HashMap<>();
+        final Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+
+        Node cloneEntry = null;
+
+        while(!queue.isEmpty()) {
+            Node original = queue.poll();
+            valCloneNodeMap.putIfAbsent(original.val, new Node(original.val));
+
+            Node clone = valCloneNodeMap.get(original.val);
+            if(cloneEntry == null) cloneEntry = clone;
+
+            for(Node neighbour : original.neighbors) {
+                if(!valCloneNodeMap.containsKey(neighbour.val)) { //We haven't seen this node so far
+                    queue.offer(neighbour);
+                }
+
+                valCloneNodeMap.putIfAbsent(neighbour.val, new Node(neighbour.val));
+                clone.neighbors.add(valCloneNodeMap.get(neighbour.val));
+            }
+        }
+
+        return cloneEntry;
+    }
+}

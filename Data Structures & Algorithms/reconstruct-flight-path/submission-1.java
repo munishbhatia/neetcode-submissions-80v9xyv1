05bb1120913f468398b8862638c699ce30c01e3e
Mickey Bhatia <munish.bhatia.cse@gmail.com>
2026-04-29 17:24:59 -0400

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        final Map<String, PriorityQueue<String>> adjList = createGraph(tickets);
+        return findEulerianCircuit(adjList, "JFK");
+    }
+
+    private Map<String, PriorityQueue<String>> createGraph(List<List<String>> tickets) {
+        final Map<String, PriorityQueue<String>> graph = new HashMap<>();
+
+        for(List<String> ticket : tickets) {
+            graph.computeIfAbsent(ticket.get(0), x -> new PriorityQueue<>()).add(ticket.get(1));
+        }
+
+        return graph;
+    }
+
+    private List<String> findEulerianCircuit(Map<String, PriorityQueue<String>> graph, String start) {
+        final Stack<String> stack = new Stack();
+        final List<String> circuit = new ArrayList<>();
+
+        stack.push(start);
+
+        while(!stack.isEmpty()) {
+            String currPort = stack.peek();
+
+            if(graph.getOrDefault(currPort, new PriorityQueue<>()).isEmpty()) {
+                circuit.add(stack.pop());
+                continue;
+            };
+
+            String dest = graph.get(currPort).poll();
+            stack.push(dest);
+        }
+
+        Collections.reverse(circuit);
+        return circuit;
+    }
+}

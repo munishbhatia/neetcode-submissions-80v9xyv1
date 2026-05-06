a9528a8e204111aab60361752118eccddee6328d
Mickey Bhatia <munish.bhatia.cse@gmail.com>
2026-05-06 13:56:24 -0400

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        if(capacity <= 0) return false;
+
+        if(trips == null || trips.length == 0) return true;
+
+        final int len = trips.length;
+        Arrays.sort(trips, Comparator.comparing((int[] a) -> a[1])); //Ordered by the trip start position
+
+        final PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing((int[] a) -> a[2])); //Ordered by trip finish position ascending
+
+        int start = trips[0][1];
+        int currCapacity = capacity;
+
+        int i = 0;
+        while(i < len && trips[i][1] == start) {
+            if(currCapacity < trips[i][0]) return false;
+            currCapacity -= trips[i][0];
+            pq.offer(trips[i]);
+            ++i;
+        }
+        
+        while(!pq.isEmpty() && i < len) {
+            while(!pq.isEmpty() && pq.peek()[2] <= trips[i][1]) {
+                // start = pq.peek()[2];
+                currCapacity += pq.poll()[0];
+            }
+
+            start = trips[i][1];
+            while(i < len && trips[i][1] <= start) {
+                if(currCapacity < trips[i][0]) return false;
+                currCapacity -= trips[i][0];
+                pq.offer(trips[i]);
+                ++i;
+            }
+        }
+
+        return true;
+    }
+}

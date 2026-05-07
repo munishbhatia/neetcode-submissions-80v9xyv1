62c071ec8b8d9a315be3a748908a2706df12565f
Mickey Bhatia <munish.bhatia.cse@gmail.com>
2026-05-07 08:10:07 -0400

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits == null || digits.length() == 0) return new ArrayList<>();
+
+        final Map<Character, List<String>> digitCharMap = getDigitCharacterMap();
+        final int len = digits.length();
+
+        List<StringBuilder> responseBuilder = new LinkedList<>();
+        for(String s : digitCharMap.get(digits.charAt(0))) {
+            responseBuilder.add(new StringBuilder(s));
+        }
+        
+        for(int i=1; i < len; ++i) {
+            List<StringBuilder> temp = new LinkedList<>();
+            for(String s : digitCharMap.get(digits.charAt(i))) {
+                for(StringBuilder sb : responseBuilder) {
+                    temp.add(new StringBuilder(sb.toString() + s));
+                }
+            }
+            responseBuilder = temp;
+        }
+
+        return responseBuilder.stream().map(StringBuilder::toString).toList();
+    }
+
+    private Map<Character, List<String>> getDigitCharacterMap() {
+        final Map<Character, List<String>> digitCharMap = new HashMap<>();
+
+        digitCharMap.put('2', List.of("a", "b", "c"));
+        digitCharMap.put('3', List.of("d", "e", "f"));
+        digitCharMap.put('4', List.of("G", "H", "I").stream().map(String::toLowerCase).toList());
+        digitCharMap.put('5', List.of("J", "K", "L").stream().map(String::toLowerCase).toList());
+        digitCharMap.put('6', List.of("M", "N", "O").stream().map(String::toLowerCase).toList());
+        digitCharMap.put('7', List.of("P", "Q", "R", "S").stream().map(String::toLowerCase).toList());
+        digitCharMap.put('8', List.of("T", "U", "V").stream().map(String::toLowerCase).toList());
+        digitCharMap.put('9', List.of("W", "X", "Y", "Z").stream().map(String::toLowerCase).toList());
+
+        return digitCharMap;
+    }
+}

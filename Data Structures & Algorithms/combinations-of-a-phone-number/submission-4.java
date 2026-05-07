f8b68ebc8c0fecb8faef65ec6c81dce580e41031
Mickey Bhatia <munish.bhatia.cse@gmail.com>
2026-05-07 08:56:22 -0400

    Add: combinations-of-a-phone-number - submission-4

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-4.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits == null || digits.length() == 0) return new ArrayList<>();
+
+        final Map<Character, List<String>> digitCharMap = getDigitCharacterMap();
+        final int len = digits.length();
+
+        final List<String> response = new LinkedList<>();
+
+        letterCombinationsBacktrack(digits, 0, len, digitCharMap, new StringBuilder(), response);
+
+        return response;
+    }
+
+    private void letterCombinationsBacktrack(String digits, int index, int len, Map<Character, List<String>> digitCharMap, StringBuilder current, List<String> response) {
+        if(index < 0) return; //Mostly a safeguard against errors
+
+        if(index == len) {
+            response.add(current.toString());
+            return;
+        }
+
+        for(String s : digitCharMap.get(digits.charAt(index))) {
+            current.append(s); //Build
+            letterCombinationsBacktrack(digits, index+1, len, digitCharMap, current, response); //Recurse
+            current.deleteCharAt(index); //Backtrack
+        }
+    }
+
+    private Map<Character, List<String>> getDigitCharacterMap() {
+        final Map<Character, List<String>> digitCharMap = new HashMap<>();
+
+        digitCharMap.put('2', List.of("a", "b", "c"));
+        digitCharMap.put('3', List.of("d", "e", "f"));
+        digitCharMap.put('4', List.of("g", "h", "i"));
+        digitCharMap.put('5', List.of("j", "k", "l"));
+        digitCharMap.put('6', List.of("m", "n", "o"));
+        digitCharMap.put('7', List.of("p", "q", "r", "s"));
+        digitCharMap.put('8', List.of("t", "u", "v"));
+        digitCharMap.put('9', List.of("w", "x", "y", "z"));
+
+        return digitCharMap;
+    }
+}
